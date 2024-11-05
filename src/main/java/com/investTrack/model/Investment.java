8553fc5acbe1d26c5e67fcbc72c16e87d6ab448c
magmarc9 <magmarc9@gmail.com>
2024-11-05 23:35:28 +0100

    Investment class scaffolding

--- src/main/java/com/investTrack/model/Investment.java
+++ src/main/java/com/investTrack/model/Investment.java
@@ -1,3 +1,41 @@
 package com.investTrack.model;
 
-public class Investment {}
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "investments")
+public class Investment {
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(length = 500)
+  private String description;
+
+  @Column(name = "platform_name", nullable = false)
+  private String platformName;
+
+  @Column(name = "start_datetime", nullable = false)
+  private LocalDateTime startDateTime;
+
+  @Column(name = "end_datetime")
+  private LocalDateTime endDateTime;
+
+  @Column(name = "is_active", nullable = false)
+  private boolean isActive;
+
+  @Column(name = "is_reinvested")
+  private boolean isReinvested;
+
+  private String currency;
+}
