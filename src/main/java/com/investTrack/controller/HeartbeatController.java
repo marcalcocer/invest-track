62c7cc31c19a213eeb75ce092c176a5fccc47bfd
magmarc9 <magmarc9@gmail.com>
2024-12-01 17:25:11 +0100

    Heartbeat endpoint

---
+++ src/main/java/com/investTrack/controller/HeartbeatController.java
@@ -0,0 +1,18 @@
+package com.investTrack.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class HeartbeatController {
+  @GetMapping("/heartbeat")
+  public ResponseEntity<String> heartbeat() {
+    log.debug("Heartbeat sent!");
+    return ResponseEntity.ok().build();
+  }
+}
