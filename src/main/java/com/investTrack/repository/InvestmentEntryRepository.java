ff3e979818777d1bdda471750d8092baba80d0cc
magmarc9 <magmarc9@gmail.com>
2024-12-01 17:26:30 +0100

    InvestmentEntry clean up
    We will use the same service, repository and controller
    for both entities.

--- src/main/java/com/investTrack/repository/InvestmentEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.investTrack.repository;
-
-import com.investTrack.model.InvestmentEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvestmentEntryRepository extends JpaRepository<InvestmentEntry, Integer> {
-  // Custom query methods (if needed) can be added here
-}
