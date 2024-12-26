7c9ae7155960fbb8c90ca6577ee7e7b19257408b
magmarc9 <magmarc9@gmail.com>
2024-12-26 14:08:08 +0100

    Create database infrastructure

--- src/main/java/com/investTrack/InvestmentTrackerApplication.java
+++ src/main/java/com/investTrack/InvestmentTrackerApplication.java
@@ -2,10 +2,8 @@ package com.investTrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// We need to exclude DataSourceAutoConfiguration to avoid an error initializing the project
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class InvestmentTrackerApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/investTrack/model/Investment.java
+++ src/main/java/com/investTrack/model/Investment.java
@@ -6,25 +6,35 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
 
-@Entity
+@Entity(name = "investments")
+@Table(name = "investments")
 @Data
 @Builder
+@AllArgsConstructor
 public class Investment {
+
+  public Investment() {
+    // Default constructor required by JPA
+  }
+
   @Id
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String name;
 
   @Column(length = 500)
   private String description;
 
-  @Column(length = 3)
+  @Column(name = "currency", length = 3, nullable = false)
   private String currency; // TODO (Marc. A): Create currency enum
 
   @Column(name = "start_datetime", nullable = false)
@@ -34,9 +44,11 @@ public class Investment {
   private LocalDateTime endDateTime;
 
   @Column(name = "is_active", nullable = false)
+  @ColumnDefault("true")
   private boolean isActive;
 
   @Column(name = "is_reinvested", nullable = false)
+  @ColumnDefault("false")
   private boolean isReinvested;
 
   @Column private double initialInvestedAmount;
