3705fbbab6f3a4c6f8e89903fd6686fa3a2c60e1
magmarc9 <magmarc9@gmail.com>
2024-12-28 23:59:37 +0100

    Create "create" endpoint and logic

--- src/main/java/com/investTrack/controller/InvestmentController.java
+++ src/main/java/com/investTrack/controller/InvestmentController.java
@@ -1,5 +1,6 @@
 package com.investTrack.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.investTrack.model.Investment;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,18 +29,31 @@ public class InvestmentController {
   @GetMapping("/reload")
   public ResponseEntity<List<Investment>> loadInvestments() {
     log.debug("Load investment endpoint called");
-    List<Investment> investments = investmentService.loadInvestments();
+    List<Investment> investments = investmentService.getInvestments();
 
     if (investments == null) {
-      log.debug("Answering with internal server error");
+      log.debug("Answering with internal server error to load investment call");
       return ResponseEntity.internalServerError().build();
     }
 
     if (investments.isEmpty()) {
-      log.debug("Answering with no content");
+      log.debug("Answering with no content to load investment call");
       return ResponseEntity.noContent().build();
     }
 
     return new ResponseEntity<>(investments, OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Investment> createInvestment(@RequestBody Investment investment) {
+    log.debug("Create investment endpoint called");
+    var savedInvestment = investmentService.createInvestment(investment);
+
+    if (savedInvestment == null) {
+      log.debug("Answering with internal server error to create investment call");
+      return ResponseEntity.internalServerError().build();
+    }
+
+    return new ResponseEntity<>(savedInvestment, CREATED);
   }
 }
--- src/test/java/com/investTrack/controller/InvestmentControllerTest.java
+++ src/test/java/com/investTrack/controller/InvestmentControllerTest.java
@@ -1,6 +1,8 @@
 package com.investTrack.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -36,27 +38,54 @@ public class InvestmentControllerTest {
   @ParameterizedTest(name = "ShouldReturn{1}When{0}Investments")
   @MethodSource("parametersForInvestments")
   public void testLoadInvestments(List<Investment> investments, HttpStatus status) {
-    doReturn(investments).when(investmentService).loadInvestments();
+    doReturn(investments).when(investmentService).getInvestments();
 
     var response = controller.loadInvestments();
 
     assertEquals(status, response.getStatusCode());
 
-    verify(investmentService).loadInvestments();
+    verify(investmentService).getInvestments();
     verifyNoMoreInteractions(investmentService);
   }
 
   @Test
   public void testLoadInvestments_ShouldReturnInvestments_WhenLoadedInvestments() {
     var investments = List.of(newInvestmentWithId(1L), newInvestmentWithId(2L));
-    doReturn(investments).when(investmentService).loadInvestments();
+    doReturn(investments).when(investmentService).getInvestments();
 
     var response = controller.loadInvestments();
 
     assertEquals(OK, response.getStatusCode());
     assertEquals(investments, response.getBody());
 
-    verify(investmentService).loadInvestments();
+    verify(investmentService).getInvestments();
+    verifyNoMoreInteractions(investmentService);
+  }
+
+  @Test
+  public void testCreateInvestment_ShouldReturnInternalServerError_WhenNullInvestment() {
+    var investment = newInvestmentWithId(1L);
+    doReturn(null).when(investmentService).createInvestment(any());
+
+    var response = controller.createInvestment(investment);
+
+    assertEquals(INTERNAL_SERVER_ERROR, response.getStatusCode());
+
+    verify(investmentService).createInvestment(eq(investment));
+    verifyNoMoreInteractions(investmentService);
+  }
+
+  @Test
+  public void testCreateInvestment_ShouldReturnCreatedInvestment_WhenInvestmentCreated() {
+    var investment = newInvestmentWithId(1L);
+    doReturn(investment).when(investmentService).createInvestment(any());
+
+    var response = controller.createInvestment(investment);
+
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    assertEquals(investment, response.getBody());
+
+    verify(investmentService).createInvestment(eq(investment));
     verifyNoMoreInteractions(investmentService);
   }
 
