832acd095460992ae39082ceb8388d615a4ad278
magmarc9 <magmarc9@gmail.com>
2025-01-19 14:20:21 +0100

    Add CORS frontend localhost validation

---
+++ src/main/java/com/investTrack/controller/WebConfig.java
@@ -0,0 +1,21 @@
+package com.investTrack.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    // For test purposes, allow localhost:4321 to access the API
+    // TODO: Improve this before publishing it to production and work with profiles
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:4321") // Frontend origin
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
+}
